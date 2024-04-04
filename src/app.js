2d0b51641ee93af0288d187a343f50b2130db098
Imkdshah <imkdshah@gmail.com>
2024-04-04 20:16:23 +0530

    routes

--- src/app.js
+++ src/app.js
@@ -14,8 +14,17 @@ app.use(express.json({
     limit : "16kb"
 }))
 
-app.use(express.static("public"))app.use(cookieParser())
+app.use(express.static("public"))
+app.use(cookieParser())
 app.use(express.urlencoded())
 app.use(cookieParser())
+
+// routes 
+
+import userRouter from './routes/user.routes.js'
+
+app.use("/api/v1/users" , userRouter)
+
+
 export {app}
 
